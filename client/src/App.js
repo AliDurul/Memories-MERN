cc6d8c66df3f41355bd27744214c830c93208356
Ali <alidrl26@gmail.com>
2023-11-03 15:48:22 +0200

    upp

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+import memories from "./images/memories.png";
+import { Grow, Grid } from "@mui/material";
+import Form from "./components/form/Form";
+import Posts from "./components/posts/Posts";
+
+import styled from "@emotion/styled";
 
 function App() {
+  const StyledAppBar = styled(AppBar)`
+    && {
+      border-radius: 15px;
+      margin: 30px 0;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+    }
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container maxWidth="lg">
+      <StyledAppBar position="static" color="inherit">
+        <Typography
+          variant="h6"
+          color={"rgba(0,183,255, 1)"}
+          sx={{ marginRight: "15px" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height={60} />
+      </StyledAppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            spacing={4}
+            justifyContent={"space-between"}
+            alignItems={"stretch"}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
